3532be4f87f577a7a762a393e8a2ea2fcaf828a5
Javi Cardenas <85462046+javi-cardenas@users.noreply.github.com>
2025-10-06 08:01:46 -0400

    add pytest and ruff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from python-dsa!")
+# from arrays import Stack
 
+def main():
+    # stack = Stack([1,2,3])
+    # print(stack.peek())
+    print("Hello, World!")
 
 if __name__ == "__main__":
     main()
